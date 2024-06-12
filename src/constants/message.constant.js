292f654c56d5c49f9e2ff09c31e622372eacc30a
yooseungyeop <yooseungyub@naver.com>
2024-06-12 17:17:44 +0900

    🩹 fix 일부오타수정 + 분리전 기존코드 인섬니아 테스트 동작확인 프리즈마연결변경

--- src/constants/message.constant.js
+++ src/constants/message.constant.js
@@ -10,15 +10,15 @@ export const MESSAGES = {
         DUPLICATED: '이미 가입 된 사용자입니다.',
       },
       PASSWORD: {
-        REQURIED: '비밀번호를 입력해 주세요.',
+        REQUIRED: '비밀번호를 입력해 주세요.',
         MIN_LENGTH: `비밀번호는 ${MIN_PASSWORD_LENGTH}자리 이상이어야 합니다.`,
       },
       PASSWORD_CONFIRM: {
-        REQURIED: '비밀번호 확인을 입력해 주세요.',
-        NOT_MACHTED_WITH_PASSWORD: '입력 한 두 비밀번호가 일치하지 않습니다.',
+        REQUIRED: '비밀번호 확인을 입력해 주세요.',
+        NOT_MATCHED_WITH_PASSWORD: '입력 한 두 비밀번호가 일치하지 않습니다.',
       },
       NAME: {
-        REQURIED: '이름을 입력해 주세요.',
+        REQUIRED: '이름을 입력해 주세요.',
       },
       UNAUTHORIZED: '인증 정보가 유효하지 않습니다.',
       JWT: {
